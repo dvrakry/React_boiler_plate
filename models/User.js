5806878097fd9d45714405315706258a32259eff
dvrakry <dv.rakry@gmail.com>
2022-05-30 09:46:32 +0900

    2022-05-30

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50,
+    maxlength: 100,
   },
   lastname: {
     type: String,
@@ -30,6 +33,47 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this; //이 This 가 userSchema를 가르킴
+
+  if (user.isModified("password")) {
+    //비밀번호가 바뀔때만 bcrypt사용
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        //hash가 암호화된 비밀번호
+        if (err) return next(err);
+        user.password = hash; //암호를 hash로 바꿈
+        next();
+      });
+    });
+  } else {
+    next(); //비밀번호 변경 안할때
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 1234567 ㅡ> 암호화된 패스워드 같은지 체크
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
